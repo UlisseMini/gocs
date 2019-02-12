d60fbd48a844346126f05d83a0882767bd5f906d
Ulisse Mini <ulisse.mini@gmail.com>
2019-02-12 15:51:04 -0500

    Remove TODO (because its done)

--- main.go
+++ main.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// TODO:
-// Create template directory from packr box if it does not exist. (~/.goc)
 package main
 
 import (
